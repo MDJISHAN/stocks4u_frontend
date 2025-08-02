4f03975073fb526e22bf3a095ae8b4206576f08c
MDJISHAN <103403364+MDJISHAN@users.noreply.github.com>
2025-08-02 16:44:25 +0530

    Update Intradayboost.js

--- stocks-frontend/src/components/Intradayboost.js
+++ stocks-frontend/src/components/Intradayboost.js
@@ -20,7 +20,7 @@ const IntradayBoost = ({ onBack }) => {
   useEffect(() => {
     const fetchBoostData = async () => {
       try {
-        const res = await fetch('https://stocks4u-project.onrender.com/top-and-low-growth'); // <-- Your API endpoint
+        const res = await fetch('http://127.0.0.1:5000/top-and-low-growth'); // <-- Your API endpoint
         const data = await res.json();
         setBoostData(data);
       } catch (err) {
